fae56dd9bcf76961b935474bed280de380fb07e5
unknown <aashish.jangam55@gmail.com>
2020-01-23 14:12:49 +0545

    Trips page completed flutter

---
+++ lib/homepage.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  Homepage({Key key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //backgroundColor: Colors.white,
+        appBar: new AppBar(
+          elevation: 0,
+          title: new Text(
+            "Ghumna Jaam",
+            style: new TextStyle(
+              color: Colors.redAccent,
+              fontFamily: "Cursive",
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+            ),
+          ),
+          //backgroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              card(),
+              card(),
+              card(),
+            ],
+          ),
+        ));
+  }
+
+  Widget card() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                width: 12.0,
+              ),
+              Container(
+                child: CircleAvatar(
+                  foregroundColor: Colors.red,
+                  backgroundImage: ExactAssetImage('assets/download.jpg'),
+                  minRadius: 10,
+                  maxRadius: 14,
+                ),
+              ),
+              SizedBox(
+                width: 15.0,
+              ),
+              Text(
+                "Hotel Name",
+                style: new TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                  fontSize: 16.0,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset("assets/hotelview1.jpg",
+            fit: BoxFit.fitWidth,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                width: 12.0,
+              ),
+              Icon(Icons.star_border,color: Colors.redAccent,size: 35.0,),
+              SizedBox(
+                width: 12.0,
+              ),
+              Icon(Icons.insert_comment,color: Colors.black87,size: 35.0,),
+            ],
+          ),
+          SizedBox(
+            height: 3.0,
+          ),
+          Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+          style: new TextStyle(
+            color: Colors.black45,
+          ),
+          
+          textAlign: TextAlign.justify,
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
